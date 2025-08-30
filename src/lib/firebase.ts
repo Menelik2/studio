a20087f5437bf039f2c15371f820b8df3abfdc40
Menelik Admasu <menelikadmasu8@gmail.com>
2025-08-30 20:48:49 +0000

    in firebase add this const firebaseConfig = {   apiKey: "AIzaSyAKNbEQPy

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAKNbEQPyzIwO7iBMT786rUSJTfgzKPbqE",
+  authDomain: "local-library-lore.firebaseapp.com",
+  projectId: "local-library-lore",
+  storageBucket: "local-library-lore.appspot.com",
+  messagingSenderId: "32992905775",
+  appId: "1:32992905775:web:db05d00519b1fc1c3e54da"
 };
 
 // Initialize Firebase
